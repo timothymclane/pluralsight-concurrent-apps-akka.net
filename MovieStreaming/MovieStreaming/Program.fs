4e41b76219db366c6f397c16df47a1107f4a8f13
Timothy McLane <timothyamclane@gmail.com>
2017-01-28 16:16:38 -0600

    Formatting cleanup.

--- MovieStreaming/MovieStreaming/Program.fs
+++ MovieStreaming/MovieStreaming/Program.fs
@@ -1,10 +1,7 @@
 ﻿namespace MovieStreaming
-
-// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
 open Akka.FSharp
 
-type PlaybackMessage ={
+type PlaybackMessage = {
     MovieTitle: string
     UserId: int
 }
@@ -31,16 +28,19 @@ module Program =
     let main argv =
         use system = System.create "movie-streaming-actor-system" <| Configuration.load()
         printfn "Actor System Created"
-        let PlaybackActor = spawn system "playback-actor" PlaybackActor
-        PlaybackActor <! { MovieTitle = "Akka.NET of Things"; UserId = 42}
-        printfn "Playback Actor Spawned"
 
-        let SimplePlaybackActor = spawn system "simple-playback-actor" <| actorOf SimplePlaybackActor
+        let playbackActor = spawn system "playback-actor" PlaybackActor
+        printfn "Playback Actor Spawned"
+        playbackActor <! { MovieTitle = "Akka.NET of Things"; UserId = 42}
+
+        let simplePlaybackActor = spawn system "simple-playback-actor" <| actorOf SimplePlaybackActor
         printfn "Simple Playback Actor Spawned"
-        SimplePlaybackActor <! { MovieTitle = "Akka.NET of Things Strikes Back"; UserId = 43}
-        SimplePlaybackActor <! { MovieTitle = "Akka.NET of Things Returns and Strikes Back Again"; UserId = 44}
+        simplePlaybackActor <! { MovieTitle = "Akka.NET of Things Strikes Back"; UserId = 43}
+        simplePlaybackActor <! { MovieTitle = "Akka.NET of Things Returns and Strikes Back Again"; UserId = 44}
 
         Console.ReadLine() |> ignore
         system.Terminate() |> Async.AwaitIAsyncResult |> ignore
-        printfn "system terminated"
-        0 // return an integer exit code
+        printfn "system terminated"      
+
+        Console.ReadLine() |> ignore
+        0
