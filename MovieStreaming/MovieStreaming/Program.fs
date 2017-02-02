4f76546271e706ed3d58c69261739e1193213072
Timothy McLane <timothyamclane@gmail.com>
2017-02-02 12:32:42 -0600

    Implemented object-based actor for instance methods.

--- MovieStreaming/MovieStreaming/Program.fs
+++ MovieStreaming/MovieStreaming/Program.fs
@@ -8,6 +8,8 @@ type PlaybackMessage = {
 
 module Actors =
     open Akka.FSharp.Actors
+    open Akka.Actor
+    open System
 
     let PlaybackActor (mailbox : Actor<_>)=
             let rec loop() = actor{
@@ -19,10 +21,33 @@ module Actors =
 
     let SimplePlaybackActor msg =
         printfn "Playing simple movie: %A with simple user id: %A" msg.MovieTitle msg.UserId
+    
+    //We must use the object-based actors if we want access to all the lifecycle methods
+    type ObjectPlaybackActor() =
+        inherit ReceiveActor()
+        let handleMovieMessage msg =
+            printfn "Playing object movie: %A with object user id: %A" msg.MovieTitle msg.UserId
+        do
+            base.Receive<PlaybackMessage> handleMovieMessage
+
+        override x.PreStart() =
+            printfn "ObjectPlaybackActor PreStart"
+
+        override x.PostStop() =
+            printfn "ObjectPlaybackActor PostStop"
+
+        override x.PreRestart (reason, msg) =
+            printfn "ObjectPlaybackActor restarted because %A" reason
+            base.PreRestart (reason, msg)
+            
+        override x.PostRestart reason =
+            printfn "ObjectPlaybackActor PostRestart because %A" reason
+            base.PostRestart reason
 
 module Program =
     open System
     open Actors
+    open Akka.Actor
 
     [<EntryPoint>]
     let main argv =
@@ -33,10 +58,15 @@ module Program =
         printfn "Playback Actor Spawned"
         playbackActor <! { MovieTitle = "Akka.NET of Things"; UserId = 42}
 
+        let objectPlaybackActor = spawnObj system "object-playback-actor" (<@ (fun () -> new ObjectPlaybackActor()) @>)
+        objectPlaybackActor <! {MovieTitle = "Akka.NET of OOPs Things"; UserId = 0085}
+
         let simplePlaybackActor = spawn system "simple-playback-actor" <| actorOf SimplePlaybackActor
         printfn "Simple Playback Actor Spawned"
         simplePlaybackActor <! { MovieTitle = "Akka.NET of Things Strikes Back"; UserId = 43}
         simplePlaybackActor <! { MovieTitle = "Akka.NET of Things Returns and Strikes Back Again"; UserId = 44}
+
+        objectPlaybackActor <! PoisonPill.Instance
 
         Console.ReadLine() |> ignore
         system.Terminate() |> Async.AwaitIAsyncResult |> ignore
